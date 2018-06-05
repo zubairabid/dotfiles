7d3d756622835766ea2de2bfe22d8df4ae691f0c
zubairabid <zubairabid1999+github@gmail.com>
2018-06-06 02:12:31 +0530

    Daily update: Wed Jun  6 02:12:31 IST 2018

--- cscript.sh
+++ cscript.sh
@@ -12,6 +12,12 @@ echo "Copied vimrc"
 cp -u ~/.i3/config ~/Documents/dotfiles/
 echo "Copied i3 config"
 
+echo "GIT: procedure started"
 git add --all
+echo "Added modified files"
+
 git commit -m "Daily update: `date`"
+echo "Successful commit"
+
 git push origin master
+echo "Pushed"
