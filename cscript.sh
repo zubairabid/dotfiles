6edaf266fa94fb8313688304f161a6c8d7ace54c
zubairabid <zubairabid1999+github@gmail.com>
2018-10-02 13:49:09 +0530

    modifies script, useful maybe

--- cscript.sh
+++ cscript.sh
@@ -13,11 +13,18 @@ cp -u ~/.i3/config ~/Documents/dotfiles/i3config
 echo "Copied i3 config"
 
 echo "GIT: procedure started"
+git checkout -b new
 git add --all
 echo "Added modified files"
 
 git commit -m "Daily update: `date`"
 echo "Successful commit"
 
-git push origin master
+git push origin new
 echo "Pushed"
+
+git checkout master
+
+git branch -D new
+
+git fetch origin
