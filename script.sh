fa5fadddf10bab4a34fad6d417bda8fea63a4aac
zubairabid <zubairabid1999+github@gmail.com>
2019-06-14 16:53:30 +0530

    Added mpdscribble. Removed default touch trackpad from config due to instability.

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp -ruv ~/.config/ config
+cp -ruv ~/.zcompdump ~/.zcompdump-zsys-5.7.1 ~/.zshrc ~/.zshrc.pre-oh-my-zsh ~/.zshrc.zni zsh
+cp -ruv ~/.oh-my-zsh/ zsh/oh-my-zsh
+cp -ruv ~/.ncmpcpp/ ncmpcpp
+cp -ruv ~/.mpdscribble mpdscribble
+
+echo "Done transferring files"
