354da825187ffa3b7d6458a5d7d2efabafe51fce
zubairabid <zubairabid1999+github@gmail.com>
2019-06-25 18:11:12 +0530

    transparent active

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cp -ruv ~/.config/ config
+rm -rf config
+cp -rv ~/.config/ config
 cp -ruv ~/.zcompdump ~/.zcompdump-zsys-5.7.1 ~/.zshrc ~/.zshrc.pre-oh-my-zsh ~/.zshrc.zni zsh
 cp -ruv ~/.oh-my-zsh/ zsh/oh-my-zsh
 cp -ruv ~/.ncmpcpp/ ncmpcpp
